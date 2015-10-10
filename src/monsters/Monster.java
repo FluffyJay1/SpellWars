8031d2dc54712a9916e8caa8bd867c375a727fdb
Daniel He <Daniel He@10.104.68.117>
2015-10-10 16:28:02 -0700

    Modified inoperational path finding

--- src/monsters/Monster.java
+++ src/monsters/Monster.java
@@ -9,10 +9,7 @@ import mechanic.GameElement;
 
 public class Monster extends GameElement {
 	public ArrayList<Point> pathFind(boolean[][] pathgrid, Point loc, Point target) {
-		return pathFindRec(pathgrid, loc, target, new int[pathgrid.length][pathgrid[0].length]);
-	}
-	
-	private ArrayList<Point> pathFindRec(boolean[][] pathgrid, Point loc, Point target, int[][] grid){
+		pathgrid, loc, target, new int[pathgrid.length][pathgrid[0].length]);
 		if (Point.equals(loc, target)) {
 			ArrayList<Point> solution = new ArrayList<Point>();
 			solution.add(target);
