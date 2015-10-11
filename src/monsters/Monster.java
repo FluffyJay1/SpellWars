dd3e63d3b37bf0cac4d8d955d2442f69c1cc7159
Peachball <chenyexu@gmail.com>
2015-10-10 20:23:30 -0700

    Added lasers and now you can see things when you run it!

--- src/monsters/Monster.java
+++ src/monsters/Monster.java
@@ -8,6 +8,9 @@ import mechanic.Point;
 import mechanic.GameElement;
 
 public class Monster extends GameElement {
+	public Monster(){
+		super();
+	}
 	public ArrayList<Point> pathFind(boolean[][] pathgrid, Point loc, Point target) {
 		double[][] efficiency = new double[pathgrid.length][pathgrid[0].length];
 		
@@ -16,5 +19,6 @@ public class Monster extends GameElement {
 			solution.add(target);
 			return solution;
 		}
+		return null;
 	}
 }
---
+++ src/towers/DefaultTower.java
@@ -0,0 +1,34 @@
+package towers;
+
+import org.newdawn.slick.Graphics;
+
+import mechanic.GameElement;
+import mechanic.GameMap;
+import mechanic.Point;
+import monsters.Monster;
+import projectiles.Laser;
+
+public class DefaultTower extends Tower{
+	private GameMap map;
+	public DefaultTower(double x, double y, GameMap map){
+		super();
+		this.changeLoc(new Point(x,y));
+		this.setImage("res/fatbarrelturret.JPG");
+		this.map = map;
+	}
+	
+	@Override
+	public void update(){
+		for(GameElement e : map.getElements()){
+			if(e instanceof Monster){
+				map.addProjectile(new Laser(this, e));
+			}
+		}
+	}
+	
+	@Override
+	public void draw(Graphics g){
+		g.drawImage(this.getImage().getScaledCopy((float) 0.5/* MAGIC NUMBER*/),(float)this.getX(), (float)this.getY());
+	}
+	
+}
