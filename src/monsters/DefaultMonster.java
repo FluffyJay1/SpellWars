cede830df7178fca892e10feb44617a272f8f834
Michael Yang <michaelzcyang@gmail.com>
2015-10-12 20:40:17 -0700

    Made effects prettier and added better comments

--- src/monsters/DefaultMonster.java
+++ src/monsters/DefaultMonster.java
@@ -24,11 +24,11 @@ public class DefaultMonster extends Monster{
 	public void update() {
 		if(this.getRemove()) {
 			String i = "res/explosion.png";
-			ParticleEmitter pe = new ParticleEmitter(this.getLoc(), emitterTypes.POINT, i, Color.white, true, /*point, type, color, alphaDecay*/
-					0, /*drag*/
-					-30, 30, /*rVel*/
-					0, 2, /*lifetime*/
-					1, 200, /*launch speed*/
+			ParticleEmitter pe = new ParticleEmitter(this.getLoc(), emitterTypes.POINT, i, Color.white, true, /*point, emitter type, image path, color, alphaDecay*/
+					2.5f, /*drag*/
+					-30, 30, /*rotational velocity*/
+					0, 1, /*min and max lifetime*/
+					10, 500, /*min and max launch speed*/
 					0, 40, /*emitter lifetime, emission rate (if emitter lifetime is 0, then it becomes instant and emission rate becomes number of particles)*/
 					0, 0, 0, 0, map); /*keyvalues and map*/
 			map.addParticleEmitter(pe);
